4ec94c4110f34ce744df836f922f104762972895
Xin Wang (xinwa3) <xinwa3@cisco.com>
2016-05-06 11:24:16 -0700

    change setup

--- setup.py
+++ setup.py
@@ -5,30 +5,17 @@ try:
 except ImportError:
     from distutils.core import setup
 
-with open('VERSION.txt', 'r') as v:
-    version = v.read().strip()
-
-with open('REQUIREMENTS.txt', 'r') as r:
-    requires = r.read().split()
-
-with open('README.rst', 'r') as r:
-    readme = r.read()
-
-download_url = (
-    'https://github.com/juliomalegria/python-craigslist/tarball/%s'
-)
 
 
 setup(
-    name='python-craigslist',
+    name='craiglistwrappertest',
     packages=['craigslist'],
-    version=version,
+    version=1.0.0,
     description=('Simple Craigslist wrapper.'),
     long_description=readme,
-    author='Julio M Alegria',
-    author_email='juliomalegria@gmail.com',
-    url='https://github.com/juliomalegria/python-craigslist',
-    download_url=download_url % version,
+    author='original Julio M Alegria',
+    author_email='test@test.com',
+    url='https://github.com/xinswang/craiglistwrappertest',
     install_requires=requires,
     license='MIT-Zero'
 )
